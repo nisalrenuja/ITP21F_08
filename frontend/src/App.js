a344e5659c5ae298c3c6fbff15a719be6e87642a
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-07-23 21:56:23 +0530

    hero section and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Navbar from './components/Navbar';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/HomePage/Home";
+import Footer from "./components/pages/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/'/>
+        <Route path="/" exact component={Home} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
+const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
 
-const COLOR = ['primary', 'blue', 'red', 'green'];
+const COLOR = ["primary", "blue", "red", "green"];
 
 export const Button = ({
   children,
@@ -13,7 +13,7 @@ export const Button = ({
   onClick,
   buttonStyle,
   buttonSize,
-  buttonColor
+  buttonColor,
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
