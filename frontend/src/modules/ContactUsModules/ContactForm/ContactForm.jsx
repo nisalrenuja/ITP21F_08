b558a04a924b74928dcd3992cdf2eeb5267224b7
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-07-26 20:05:48 +0530

    #comment links added

--- frontend/src/modules/ContactUsModules/ContactForm/ContactForm.jsx
+++ frontend/src/modules/ContactUsModules/ContactForm/ContactForm.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import 'react-notifications/lib/notifications.css';
-import { NotificationContainer, NotificationManager } from 'react-notifications';
-import './ContactForm.css';
-import emailjs from 'emailjs-com';
-import ContactUsImg from '../../../assets/contactus/contactus.svg';
+import React from "react";
+import "react-notifications/lib/notifications.css";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import "./ContactForm.css";
+import emailjs from "emailjs-com";
+import ContactUsImg from "../../../assets/contactus/contact-us.svg";
 
 const sendEmail = (e) => {
   e.preventDefault();
-  NotificationManager.info('Please wait..');
-  emailjs.sendForm('service_504yqmn', 'template_9ine6vh', e.target, 'user_OkeRUw8xijd6xXRflprac').then(
-    () => {
-      NotificationManager.success('Email sent successfully');
-      document.getElementById('myForm').reset();
-    },
-    () => {
-      NotificationManager.warning('Something went wrong');
-    },
-  );
+  NotificationManager.info("Please wait..");
+  emailjs
+    .sendForm(
+      "service_504yqmn",
+      "template_9ine6vh",
+      e.target,
+      "user_OkeRUw8xijd6xXRflprac"
+    )
+    .then(
+      () => {
+        NotificationManager.success("Email sent successfully");
+        document.getElementById("myForm").reset();
+      },
+      () => {
+        NotificationManager.warning("Something went wrong");
+      }
+    );
 };
 
 const ContactForm = () => (
@@ -35,19 +45,36 @@ const ContactForm = () => (
                 <label htmlFor="name" className="text-secondary">
                   Your Name
                 </label>
-                <input id="name" name="name" type="text" className="form-control" placeholder="Enter Your Name" />
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Your Name"
+                />
               </div>
               <div className="form-group mt-3">
                 <label htmlFor="email" className="text-secondary">
                   Email address
                 </label>
-                <input id="email" name="email" type="email" className="form-control" placeholder="Enter Your Email" />
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                />
               </div>
               <div className="form-group mt-3">
                 <label htmlFor="message" className="text-secondary">
                   Message
                 </label>
-                <textarea id="message" name="message" className="form-control" placeholder="Enter Your Message" />
+                <textarea
+                  id="message"
+                  name="message"
+                  className="form-control"
+                  placeholder="Enter Your Message"
+                />
               </div>
               <div className="form-group mt-3">
                 <button type="submit" className="contact-btn">
--- frontend/src/modules/EventsModule/heroSection/EventHeroSection.jsx
+++ frontend/src/modules/EventsModule/heroSection/EventHeroSection.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './EventHeroSection.css';
-import EventHeroHeaderImage from '../../../assets/flat-illustrations/undraw_events_2p66 1.png';
+import React from "react";
+import "./EventHeroSection.css";
+import EventHeroHeaderImage from "../../../assets/flat-illustrations/undraw_events_2p66 1.png";
 
 const EventHeroSection = () => (
   <div className="container-fluid header">
@@ -8,7 +8,7 @@ const EventHeroSection = () => (
     <div className="row">
       <div className="col-sm-12 col-md-6 col-lg-6 text-div">
         <div className="col-12">
-          <p className="exploreText">Explore events</p>
+          <p className="exploreText">Explore Notices</p>
         </div>
         <div className="col-12 event-search">
           <input type="text" className="form-control searchBox" />
@@ -18,7 +18,11 @@ const EventHeroSection = () => (
         </div>
       </div>
       <div className="col-sm-12 col-md-6 col-lg-6 d-flex justify-content-center align-item-center">
-        <img src={EventHeroHeaderImage} alt="Event header img" className="event-header-img" />
+        <img
+          src={EventHeroHeaderImage}
+          alt="Event header img"
+          className="event-header-img"
+        />
       </div>
     </div>
   </div>
