19fb3006256cc5f34c6eea41b8d72cf6ce3df4ec
sajinikulara <kulari102@gmail.com>
2021-08-20 19:57:23 +0530

    #Add Laptop Inventory Details to backend

---
+++ backend/models/laptop_repair.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const laptop_repair_Schema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  repair_reason: {
+    type: String,
+    required: true,
+  },
+  
+
+});
+module.exports = mongoose.model("laptops_repair", laptop_repair_Schema);
+
