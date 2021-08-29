4eaf4cf8eb4641d2f2bb9fccac82c204f5b7ae9a
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-08-29 23:43:03 +0530

    major hotfixes in frontend

---
+++ frontend/src/modules/AdminModules/AdminTab4.jsx
@@ -0,0 +1,5 @@
+const AdminTab4 = () => {
+  return <div>Tab4 content goes here</div>;
+};
+
+export default AdminTab4;
---
+++ frontend/src/modules/AdminModules/AdminTab7.jsx
@@ -0,0 +1,5 @@
+const AdminTab7 = () => {
+  return <div>Tab7 content goes here</div>;
+};
+
+export default AdminTab7;
