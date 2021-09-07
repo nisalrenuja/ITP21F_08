d80ee9d618024d2e6ec4943b6c8ca6f879b58590
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-09-07 23:18:46 +0530

    review page upgrades

--- backend/models/Reviews.js
+++ backend/models/Reviews.js
@@ -9,10 +9,10 @@ const review_Schema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  //imgPath: {
-  //  type: String,
-  //  required: true,
-  // },
+  reportPDF: {
+    type: String,
+    required: true,
+  },
   points: {
     type: String,
     required: true,
