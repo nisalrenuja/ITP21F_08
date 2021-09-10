4b928ad05490d8614bcb0671aaa64d669a290978
thisarani12 <thisaranijayawardhana97@gmail.com>
2021-09-11 03:50:04 +0530

    #modified ../backend/models/client.js

--- backend/models/client.js
+++ backend/models/client.js
@@ -2,64 +2,16 @@
 const mongoose = require("mongoose");
 
 const client_Schema = new mongoose.Schema({
-    clientno: {
+        clientID: {
         type: String,
         unique: true,
         required: true,
       },
-      clientName: {
+      company_name: {
         type: String,
         required: true,
       },
-      org_type: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-      },
-      tel_no: [
-        { tag:"contact 1",
-          type: String,
-          required: true,
-        },
-        { tag:"contact 2",
-          type: String,
-          required: true,
-        }
-      ],
-      affiliated_comp_name: [{
-        type: String,
-        required: true,
-      }],
-     
-      
-      dirname: {
-        type: String,
-        required: true,
-      },
-
-      dirtel: {
-        type: String,
-        required: true,
-      },
-
-      dirtemail: {
-        type: String,
-        required: true,
-      },
-
-      position: {
-        type: String,
-        required: true,
-      },
-
-
-
-
-
-      address: {
+      location: {
         type: String,
         required: true,
       },
@@ -68,36 +20,7 @@ const client_Schema = new mongoose.Schema({
         required: true,
         
       },
-      tex_fee: {
-        type: String,
-        required: true,
-      },
-      business_status: {
-        type: String,
-        required: true,
-      },
-
-      final_report_issured_on: {
-        type: String,
-        required: true,
-      },
-
-      commencement_date: {
-        type: String,
-        required: true,
-      },
-
-      audit_fee: {
-        type: String,
-        required: true,
-      },
-
-      date_paid: {
-        type: String,
-        required: true,
-      },
-
-      report_period: {
+      status: {
         type: String,
         required: true,
       },
