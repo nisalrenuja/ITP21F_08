788e35f6f86473b7145c1a3de99d3473718dae74
SenaraM <senaramallikahewa@gmail.com>
2021-10-12 14:22:17 +0530

    Minor changes

--- frontend/src/modules/AdminModules/AdminTab5.jsx
+++ frontend/src/modules/AdminModules/AdminTab5.jsx
@@ -6,7 +6,7 @@ export default class AdminTab5 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      existingNotices: [] //CreateNotice
+      existingNotices: []
     };
   }
 
